04cafb134b559f4e52da9740b26051d590a63a84
Nicholas Niro <blowfist@xroutine.net>
2019-10-26 11:02:35 -0400

    newJail.sh: startRoot - Fixed an out of jail bug.
    This bug was introduced in the new patch about sending commands
    when out of the jail directory. We add a function which is required
    to do verifications.

--- newJail.sh
+++ newJail.sh
@@ -244,6 +244,14 @@ fi
 
 ownPath=\$(dirname \$0)
 
+# substring offset <optional length> string
+# cuts a string at the starting offset and wanted length.
+substring() {
+        local init=\$1; shift
+        if [ "\$2" != "" ]; then toFetch="\(.\{\$1\}\).*"; shift; else local toFetch="\(.*\)"; fi
+        echo "\$1" | sed -e "s/^.\{\$init\}\$toFetch$/\1/"
+}
+
 # convert the path of this script to an absolute path
 if [ "\$ownPath" = "." ]; then
 	ownPath=\$PWD
