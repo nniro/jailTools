3ea296f07603c8765310e00241912124b5d3fb91
Nicholas Niro <blowfist@xroutine.net>
2020-04-18 21:26:42 -0400

    test-runJail: Added a test for when a jail should fail.

--- tests/runJail/test.sh
+++ tests/runJail/test.sh
@@ -25,8 +25,21 @@ echo "Attempting to re-enter the daemonized jail"
 echo exit | $jtPath shell || exit 1
 echo "Stopping the daemonized jail"
 $jtPath stop || exit 1
+sleep 1
 
 echo "Starting a jail with the shell command"
 echo exit | $jtPath shell || exit 1
+
+echo "Now we start a new jail and expect it to actually fail"
+sed -e 's/^\([[:space:]]*runJail \$rootDir\).*$/\1 fusionReactorStarter ignite ahahahah/' -i rootCustomConfig.sh
+#echo exit | $jtPath start 2>&1 && exit 1
+echo exit | $jtPath start && exit 1
+
+
+if (($err = 0)) ; then
+	echo "Error should be anything else than 0 (which it is)"
+	grep "[[:space:]]*runJail \$rootDir" rootCustomConfig.sh
+	exit 1
+fi
 
 exit 0
