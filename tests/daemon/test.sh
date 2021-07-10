2393d0bdf98ae4bf7fcc21eb074339bfbefffbf8
Nicholas Niro <blowfist@xroutine.net>
2021-07-10 16:40:48 -0400

    tests/daemon: Cleaned up the code and cleared it.
    Also converted the test segments to use the new configuration TUI of jt.

--- tests/daemon/test.sh
+++ tests/daemon/test.sh
@@ -15,89 +15,97 @@ sh=$1
 testPath=$2
 jtPath=$3
 
+jail=$testPath/basic
+
+resetConfig() {
+	jail=$1
+
+	cp $jail/._rootCustomConfig.sh.initial $jail/rootCustomConfig.sh
+}
+
 doCheck() {
-	echo giving 5 seconds maximum timeout for the jail to start the daemon
-	timeout 5 sh -c 'while :; do if [ -e run/jail.pid ]; then break; fi ; done'
-	echo 5 seconds timeout is done
-	if [ ! -e run/jail.pid ]; then
-		echo "The daemonized jail is not running, run/jail.pid contains : '$(cat run/jail.pid)'" 2>&1
+	jail=$1
+	echo giving 2 seconds maximum timeout for the jail to start the daemon
+	timeout 2 sh -c 'while :; do if [ -e run/jail.pid ]; then break; fi ; done'
+	echo 2 seconds timeout is done
+	if [ ! -e $jail/run/jail.pid ]; then
+		echo "The daemonized jail is not running, run/jail.pid contains : '$(cat $jail/run/jail.pid)'" 2>&1
 		exit 1
 	fi
 	echo "the jail is running"
 
 	echo "Testing to see if the httpd service is running"
-	sleep 1
-	pstree $(cat run/jail.pid) | grep -q httpd
+	pstree $(cat $jail/run/jail.pid) | grep -q httpd
 	err=$?
 
 	if [ "$err" != "0" ]; then
 		echo "Failed, the httpd service is not running"
-		echo "here is the info on the running process : \"$(pstree $(cat run/jail.pid))\""
+		echo "here is the info on the running process : \"$(pstree $(cat $jail/run/jail.pid))\""
+		echo "Jail processes : $($jtPath status $jail -p)"
 		echo "here is the line in rootCustomConfig.sh :"
-		grep '^daemonCommand=' rootCustomConfig.sh
-		echo "jail pid : $(cat run/jail.pid) - ns pid : $(cat run/ns.pid)"
+		grep '^daemonCommand=' $jail/rootCustomConfig.sh
+		echo "jail pid : $(cat $jail/run/jail.pid) - ns pid : $(cat $jail/run/ns.pid)"
 		echo "stopping the jail"
-		$jtPath stop 2>&1
+		$jtPath stop $jail 2>&1
 		exit 1
 	fi
 	echo "success"
 }
 
-$jtPath new $testPath/basic 2>&1 || exit 1
-cd $testPath/basic
+$jtPath new $jail >/dev/null 2>&1 || exit 1
 
 echo "Testing standard daemon with a direct command put in the background"
-cp ._rootCustomConfig.sh.initial rootCustomConfig.sh
-sed -e 's/jailNet=true/jailNet=false/' -i rootCustomConfig.sh
-sed -e 's/^\(daemonCommand=\)""$/\1"\x2Fusr\x2Fsbin\x2Fhttpd -p 8000"/' -i rootCustomConfig.sh
-$jtPath daemon 2>&1 || exit 1
-doCheck
-$jtPath stop 2>&1
-sleep 1
+$jtPath config $jail -s jailNet true
+$jtPath config $jail -s disableUnprivilegedNetworkNamespace false
+$jtPath config $jail -s daemonCommand "/usr/sbin/httpd -p 8000"
+timeout 5 $jtPath daemon $jail 2>&1 || exit 1
+doCheck $jail
+$jtPath stop $jail 2>&1
+resetConfig $jail
 
 echo "Testing standard daemon with a direct command put in the foreground"
-cp ._rootCustomConfig.sh.initial rootCustomConfig.sh
-sed -e 's/jailNet=true/jailNet=false/' -i rootCustomConfig.sh
-sed -e 's/^\(daemonCommand=\)""$/\1"\x2Fusr\x2Fsbin\x2Fhttpd -p 8000 -f"/' -i rootCustomConfig.sh
-$jtPath daemon 2>&1 || exit 1
-doCheck
-$jtPath stop 2>&1
-sleep 1
+$jtPath config $jail -s jailNet true
+$jtPath config $jail -s disableUnprivilegedNetworkNamespace false
+$jtPath config $jail -s daemonCommand "/usr/sbin/httpd -p 8000 -f"
+timeout 5 $jtPath daemon $jail 2>&1 || exit 1
+doCheck $jail
+$jtPath stop $jail 2>&1
+resetConfig $jail
 
 echo "Testing daemon with a shell that calls the command single quoted and is put in the background"
-cp ._rootCustomConfig.sh.initial rootCustomConfig.sh
-sed -e 's/jailNet=true/jailNet=false/' -i rootCustomConfig.sh
-sed -e 's/^\(daemonCommand=\)""$/\1"sh -c \x27\x2Fusr\x2Fsbin\x2Fhttpd -p 8000\x27"/' -i rootCustomConfig.sh
-$jtPath daemon 2>&1 || exit 1
-doCheck
-$jtPath stop 2>&1
-sleep 1
+$jtPath config $jail -s jailNet true
+$jtPath config $jail -s disableUnprivilegedNetworkNamespace false
+$jtPath config $jail -s daemonCommand "sh -c '/usr/sbin/httpd -p 8000'"
+timeout 5 $jtPath daemon $jail 2>&1 || exit 1
+doCheck $jail
+$jtPath stop $jail 2>&1
+resetConfig $jail
 
 echo "Testing daemon with a shell that calls the command single quoted and is put in the foreground"
-cp ._rootCustomConfig.sh.initial rootCustomConfig.sh
-sed -e 's/jailNet=true/jailNet=false/' -i rootCustomConfig.sh
-sed -e 's/^\(daemonCommand=\)""$/\1"sh -c \x27\x2Fusr\x2Fsbin\x2Fhttpd -p 8000 -f\x27"/' -i rootCustomConfig.sh
-$jtPath daemon 2>&1 || exit 1
-doCheck
-$jtPath stop 2>&1
-sleep 1
+$jtPath config $jail -s jailNet true
+$jtPath config $jail -s disableUnprivilegedNetworkNamespace false
+$jtPath config $jail -s daemonCommand "sh -c '/usr/sbin/httpd -p 8000 -f'"
+timeout 5 $jtPath daemon $jail 2>&1 || exit 1
+doCheck $jail
+$jtPath stop $jail 2>&1
+resetConfig $jail
 
 echo "Testing daemon with a shell that calls the command double quoted and is put in the background"
-cp ._rootCustomConfig.sh.initial rootCustomConfig.sh
-sed -e 's/jailNet=true/jailNet=false/' -i rootCustomConfig.sh
-sed -e 's/^\(daemonCommand=\)""$/\1"sh -c \\\x22\x2Fusr\x2Fsbin\x2Fhttpd -p 8000\\\x22"/' -i rootCustomConfig.sh
-$jtPath daemon 2>&1 || exit 1
-doCheck
-$jtPath stop 2>&1
-sleep 1
+$jtPath config $jail -s jailNet true
+$jtPath config $jail -s disableUnprivilegedNetworkNamespace false
+$jtPath config $jail -s daemonCommand "sh -c \"\"/usr/sbin/httpd -p 8000\"\""
+timeout 5 $jtPath daemon $jail 2>&1 || exit 1
+doCheck $jail
+$jtPath stop $jail 2>&1
+resetConfig $jail
 
 echo "Testing daemon with a shell that calls the command double quoted and is put in the foreground"
-cp ._rootCustomConfig.sh.initial rootCustomConfig.sh
-sed -e 's/jailNet=true/jailNet=false/' -i rootCustomConfig.sh
-sed -e 's/^\(daemonCommand=\)""$/\1"sh -c \\\x22\x2Fusr\x2Fsbin\x2Fhttpd -p 8000 -f\\\x22"/' -i rootCustomConfig.sh
-$jtPath daemon 2>&1 || exit 1
-doCheck
-$jtPath stop 2>&1
-sleep 1
+$jtPath config $jail -s jailNet true
+$jtPath config $jail -s disableUnprivilegedNetworkNamespace false
+$jtPath config $jail -s daemonCommand "sh -c \"\"/usr/sbin/httpd -p 8000 -f\"\""
+timeout 5 $jtPath daemon $jail 2>&1 || exit 1
+doCheck $jail
+$jtPath stop $jail 2>&1
+resetConfig $jail
 
 exit 0
