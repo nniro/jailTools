732c2a24075805c16b6543a9b05a4243ff9ed2d3
Nicholas Niro <blowfist@xroutine.net>
2021-08-04 14:31:04 -0400

    tests/jtConfig: Better specified the tests and added some more.
    The tests are now made with more specific regex rules. Also added some
    more quotes tests.

--- tests/jtConfig/test.sh
+++ tests/jtConfig/test.sh
@@ -16,7 +16,7 @@ echo "Checking if the command 'config' exists"
 $jtPath config 2>&1 | grep -q 'Invalid command' && exit 1
 
 echo "Checking if the latest jail config can do config"
-$jtPath config --get networking | grep -q 'true' || exit 1
+$jtPath config --get networking | grep -q '^true$' || exit 1
 
 echo "done a backup of rootCustomConfig.sh. Will manipulate 'Command part' to be like it was before the upgrade."
 cp rootCustomConfig.sh ._rootCustomConfig.sh.bak
@@ -34,28 +34,28 @@ $jtPath config --get fooBarAvecDuBeurre >/dev/null 2>/dev/null && exit 1
 
 echo "Checking basic configurations"
 $jtPath config --get jailName >/dev/null 2>/dev/null || exit 1
-$jtPath config --get extIp | grep -q '172.16.0.1' || exit 1
-$jtPath config --default --get extIp | grep -q '172.16.0.1' || exit 1
+$jtPath config --get extIp | grep -q '^172.16.0.1$' || exit 1
+$jtPath config --default --get extIp | grep -q '^172.16.0.1$' || exit 1
 
 echo "Checking various default configurations"
 echo "default jailNet"
-$jtPath config --default --get jailNet | grep -q 'true' || exit 1
+$jtPath config --default --get jailNet | grep -q '^true$' || exit 1
 echo "default createBridge"
-$jtPath config --default --get createBridge | grep -q 'false' || exit 1
+$jtPath config --default --get createBridge | grep -q '^false$' || exit 1
 echo "default networking"
-$jtPath config --default --get networking | grep -q 'false' || exit 1
+$jtPath config --default --get networking | grep -q '^false$' || exit 1
 echo "default availableDevices"
-$jtPath config --default --get availableDevices | grep -q "null urandom zero" || exit 1
+$jtPath config --default --get availableDevices | grep -q "^null urandom zero$" || exit 1
 echo "default mountSys"
-$jtPath config --default --get mountSys | grep -q 'true' || exit 1
+$jtPath config --default --get mountSys | grep -q '^true$' || exit 1
 echo "default setNetAccess"
-$jtPath config --default --get setNetAccess | grep -q 'false' || exit 1
+$jtPath config --default --get setNetAccess | grep -q '^false$' || exit 1
 
 echo "Checking various custom configurations"
 echo networking
-$jtPath config --get networking | grep -q 'true' || exit 1
+$jtPath config --get networking | grep -q '^true$' || exit 1
 echo setNetAccess
-$jtPath config --get setNetAccess | grep -q 'false' || exit 1
+$jtPath config --get setNetAccess | grep -q '^false$' || exit 1
 echo "devMountPoints"
 devMountPoints=$(cat << EOF
 /dev/dri
@@ -64,25 +64,25 @@ devMountPoints=$(cat << EOF
 /dev/shm
 EOF
 )
-$jtPath config --get devMountPoints | grep -q "$devMountPoints" || exit 1
+$jtPath config --get devMountPoints | grep -q "^$devMountPoints$" || exit 1
 
 echo "Changing a basic configuration"
 echo "extIp"
 $jtPath config --set extIp 172.16.1.1 || exit 1
-$jtPath config --get extIp | grep -q '172.16.1.1' || exit 1
-$jtPath config --default --get extIp | grep -q '172.16.0.1' || exit 1
+$jtPath config --get extIp | grep -q '^172.16.1.1$' || exit 1
+$jtPath config --default --get extIp | grep -q '^172.16.0.1$' || exit 1
 echo "setNetAccess"
 $jtPath config --set setNetAccess "true" || exit 1
-$jtPath config --get setNetAccess | grep -q 'true' || exit 1
-$jtPath config --default --get setNetAccess | grep -q 'false' || exit 1
+$jtPath config --get setNetAccess | grep -q '^true$' || exit 1
+$jtPath config --default --get setNetAccess | grep -q '^false$' || exit 1
 echo "mountSys (have to add to the custom config)"
 $jtPath config --set mountSys "false" || exit 1
-$jtPath config --get mountSys | grep -q 'false' || exit 1
-$jtPath config --default --get mountSys | grep -q 'true' || exit 1
+$jtPath config --get mountSys | grep -q '^false$' || exit 1
+$jtPath config --default --get mountSys | grep -q '^true$' || exit 1
 echo "createBridge (have to add to the custom config)"
 $jtPath config --set createBridge "true" || exit 1
-$jtPath config --get createBridge | grep -q 'true' || exit 1
-$jtPath config --default --get createBridge | grep -q 'false' || exit 1
+$jtPath config --get createBridge | grep -q '^true$' || exit 1
+$jtPath config --default --get createBridge | grep -q '^false$' || exit 1
 echo "roMountPoints"
 new_roMountPoints=$(cat << EOF
 /ahah/bleh
@@ -92,31 +92,35 @@ new_roMountPoints=$(cat << EOF
 EOF
 )
 $jtPath config --set roMountPoints "$new_roMountPoints" || exit 1
-$jtPath config --get roMountPoints | grep -q "$new_roMountPoints" || exit 1
+$jtPath config --get roMountPoints | grep -q "^$new_roMountPoints$" || exit 1
 echo "daemonCommand"
 $jtPath config --set daemonCommand "/usr/sbin/httpd -p 8000" || exit 1
-$jtPath config --get daemonCommand | grep -q '/usr/sbin/httpd -p 8000' || exit 1
+$jtPath config --get daemonCommand | grep -q '^/usr/sbin/httpd -p 8000$' || exit 1
+echo "daemonCommand without quotes"
+$jtPath config --set daemonCommand "nothing here" || exit 1
+$jtPath config --set daemonCommand -- /usr/sbin/httpd -p 8000 || exit 1
+$jtPath config --get daemonCommand | grep -q '^/usr/sbin/httpd -p 8000$' || exit 1
 
 echo "Checking various default configurations again"
 echo "default jailNet"
-$jtPath config --default --get jailNet | grep -q 'true' || exit 1
+$jtPath config --default --get jailNet | grep -q '^true$' || exit 1
 echo "default createBridge"
-$jtPath config --default --get createBridge | grep -q 'false' || exit 1
+$jtPath config --default --get createBridge | grep -q '^false$' || exit 1
 echo "default networking"
-$jtPath config --default --get networking | grep -q 'false' || exit 1
+$jtPath config --default --get networking | grep -q '^false$' || exit 1
 echo "default availableDevices"
-$jtPath config --default --get availableDevices | grep -q "null urandom zero" || exit 1
+$jtPath config --default --get availableDevices | grep -q "^null urandom zero$" || exit 1
 echo "default mountSys"
-$jtPath config --default --get mountSys | grep -q 'true' || exit 1
+$jtPath config --default --get mountSys | grep -q '^true$' || exit 1
 echo "default setNetAccess"
-$jtPath config --default --get setNetAccess | grep -q 'false' || exit 1
+$jtPath config --default --get setNetAccess | grep -q '^false$' || exit 1
 
 echo "Checking various custom configurations again"
 echo networking
-$jtPath config --get networking | grep -q 'true' || exit 1
+$jtPath config --get networking | grep -q '^true$' || exit 1
 echo setNetAccess to default
 $jtPath config -d --set setNetAccess || exit 1
-$jtPath config --get setNetAccess | grep -q 'false' || exit 1
+$jtPath config --get setNetAccess | grep -q '^false$' || exit 1
 echo "devMountPoints"
 devMountPoints=$(cat << EOF
 /dev/dri
@@ -125,10 +129,28 @@ devMountPoints=$(cat << EOF
 /dev/shm
 EOF
 )
-$jtPath config --get devMountPoints | grep -q "$devMountPoints" || exit 1
+$jtPath config --get devMountPoints | grep -q "^$devMountPoints$" || exit 1
 
 echo "with escaped double quotes"
 $jtPath config --set daemonCommand "sh -c \"/usr/sbin/httpd -p 8000\"" || exit 1
-$jtPath config --get daemonCommand | grep -q 'sh -c "/usr/sbin/httpd -p 8000"' || exit 1
+$jtPath config --get daemonCommand
+$jtPath config --get daemonCommand | grep -q "^sh -c '/usr/sbin/httpd -p 8000'$" || exit 1
+
+echo "with double quotes inside single quotes"
+$jtPath config --set daemonCommand 'sh -c "/usr/sbin/httpd -p 8000"' || exit 1
+$jtPath config --get daemonCommand | grep -q "^sh -c '/usr/sbin/httpd -p 8000'$" || exit 1
+
+echo "without englobing quotes"
+$jtPath config --set daemonCommand -- sh -c id || exit 1
+$jtPath config --get daemonCommand | grep -q '^sh -c id$' || exit 1
+
+echo "without englobing quotes but with inner double quotes"
+$jtPath config --set daemonCommand -- sh -c "/usr/sbin/httpd -p 8000" || exit 1
+$jtPath config --get daemonCommand
+$jtPath config --get daemonCommand | grep -q "^sh -c '/usr/sbin/httpd -p 8000'$" || exit 1
+
+echo "without englobing quotes but with inner single quotes"
+$jtPath config --set daemonCommand -- sh -c '/usr/sbin/httpd -p 8000' || exit 1
+$jtPath config --get daemonCommand | grep -q "^sh -c '/usr/sbin/httpd -p 8000'$" || exit 1
 
 exit 0
