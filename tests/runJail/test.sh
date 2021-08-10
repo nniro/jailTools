175107aeeb63bd98d3074a5a33ff31a4dead1dd9
Nicholas Niro <blowfist@xroutine.net>
2021-08-10 16:07:38 -0400

    tests/runJail: Fixed program calls to use busybox and some other fixes.

--- tests/runJail/test.sh
+++ tests/runJail/test.sh
@@ -6,15 +6,17 @@ sh=$1
 testPath=$2
 jtPath=$3
 
+bb=$testPath/../bin/busybox
+
 $jtPath new $testPath/basic 2>&1 || exit 1
 cd $testPath/basic
 sed -e 's/jailNet=true/jailNet=false/' -i rootCustomConfig.sh
 echo starting the jail
-echo exit | $jtPath start 2>&1 || exit 1
+$jtPath start sh -c exit 2>&1 || exit 1
 
 echo Starting a daemon
 $jtPath daemon 2>&1 || exit 1
-timeout 5 sh -c 'while :; do if [ -e run/jail.pid ]; then break; fi ; done'
+$bb timeout 5 $bb sh -c 'while :; do if [ -e run/jail.pid ]; then break; fi ; done'
 
 if [ ! -e run/jail.pid ]; then
 	echo "The daemonized jail is not running, run/jail.pid is missing"
@@ -22,17 +24,17 @@ if [ ! -e run/jail.pid ]; then
 fi
 
 echo "Attempting to re-enter the daemonized jail"
-echo exit | $jtPath shell 2>&1 || exit 1
+$jtPath shell sh -c exit 2>&1 || exit 1
 echo "Stopping the daemonized jail"
 $jtPath stop 2>&1 || exit 1
 sleep 1
 
-echo "Starting a jail with the shell command"
-echo exit | $jtPath shell 2>&1 || exit 1
+echo "Starting a jail with the shell command should not work as is"
+$jtPath shell sh -c exit 2>&1 && exit 1
 
 echo "Now we start a new jail and expect it to actually fail"
 sed -e 's/^\(startCommand=\)""$/\1"fusionReactorStarter ignite ahahahah"/' -i rootCustomConfig.sh
-echo exit | $jtPath start 2>&1 && exit 1
+$jtPath start 2>&1 && exit 1
 
 
 exit 0
