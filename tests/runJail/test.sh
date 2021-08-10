094a718ccd3c2e520b8d072451a0e6679dc097b9
Nicholas Niro <blowfist@xroutine.net>
2021-08-10 16:08:34 -0400

    tests/runJail: Added a new test checking for a bug in the shell command.
    This bug happened at a certain point where parts of the string between
    quotes would have the internal spaces converted to %20 and left that
    way. This would make it fail when it was being called.

--- tests/runJail/test.sh
+++ tests/runJail/test.sh
@@ -36,5 +36,19 @@ echo "Now we start a new jail and expect it to actually fail"
 sed -e 's/^\(startCommand=\)""$/\1"fusionReactorStarter ignite ahahahah"/' -i rootCustomConfig.sh
 $jtPath start 2>&1 && exit 1
 
+echo "We check the jail's command line feature"
+
+$jtPath daemon 2>&1 || exit 1
+$bb timeout 5 $bb sh -c 'while :; do if [ -e run/jail.pid ]; then break; fi ; done'
+
+if [ ! -e run/jail.pid ]; then
+	echo "The daemonized jail is not running, run/jail.pid is missing"
+	exit 1
+fi
+
+echo "checking if we can do multiple commands"
+$jtPath shell sh -c 'cd /usr/sbin; ls httpd' 2>/dev/null | grep -q '^httpd$' || exit 1
+$jtPath stop 2>&1 || exit 1
+sleep 1
 
 exit 0
