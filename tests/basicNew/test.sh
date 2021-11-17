2296d083b68df4be8ddc0fdb4c68e5c2deca54c8
Nicholas Niro <blowfist@xroutine.net>
2021-11-17 15:09:56 -0500

    tests/basicNew: Check for mount --bind as read-only.
    We check if directories which are mounted as read-only are really
    mounted as read-only in jail.

--- tests/basicNew/test.sh
+++ tests/basicNew/test.sh
@@ -8,6 +8,13 @@ jtPath=$3
 
 jail=$testPath/basic
 
-$jtPath new $jail 2>&1 || exit 1
+$jtPath new $jail 2>/dev/null || exit 1
+
+# we check that the bind mount to /lib is read-only and executable
+result=$($jtPath start $jail mount 2>/dev/null | grep ' \/lib ' | sed -e 's/.*\(([^)]*)\)$/\1/')
+if ! echo $result | grep -q '\((ro\|,ro,\|,ro)\)'; then
+	echo "We expect /lib to be bind mounted as read-only"
+	exit 1
+fi
 
 exit 0
