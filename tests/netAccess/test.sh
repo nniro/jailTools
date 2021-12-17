eaed81627b0fefdfa087fdec49f1bd2e2024733e
Nicholas Niro <blowfist@xroutine.net>
2021-12-17 13:25:57 -0500

    tests/netAccess: Now using a ssl aware solution.
    This should make the test much more robust.

--- tests/netAccess/test.sh
+++ tests/netAccess/test.sh
@@ -8,14 +8,14 @@ jail=$testPath/netAccess
 
 $jtPath new $jail >/dev/null 2>/dev/null || exit 1
 
-if ! $jtPath start $jail sh -c 'wget --spider -q http://1.1.1.1' 2>/dev/null; then
+if ! $jtPath start $jail sh -c 'printf "GET / HTTP/1.1\r\nHost: 1.1.1.1\r\n\r\n" | timeout 2 ssl_client 1.1.1.1 | grep -q ".*"' 2>/dev/null; then
 	echo "Could not connect to the remote site 1.1.1.1"
 	exit 1
 fi
 
 $jtPath config $jail --set setNetAccess "false" >/dev/null || exit 1
 
-if $jtPath start $jail sh -c 'wget --spider -q http://1.1.1.1' 2>/dev/null; then
+if $jtPath start $jail sh -c 'printf "GET / HTTP/1.1\r\nHost: 1.1.1.1\r\n\r\n" | timeout 2 ssl_client 1.1.1.1 | grep -q ".*"' 2>/dev/null; then
 	echo "With setNetAccess set to 'false' we should not be able to connect to the remote site 1.1.1.1"
 	exit 1
 fi
@@ -23,14 +23,14 @@ fi
 # set 1.1.1.1 as our dns provider
 echo "nameserver 1.1.1.1" > $jail/root/etc/resolv.conf
 
-if $jtPath start $jail sh -c 'wget --spider -q https://linux.org' 2>/dev/null; then
+if $jtPath start $jail sh -c 'printf "GET / HTTP/1.1\r\nHost: kernel.org\r\n\r\n" | timeout 2 ssl_client kernel.org | grep -q ".*"' 2>/dev/null; then
 	echo "With setNetAccess set to 'false' we should not be able to connect to the remote site linux.org"
 	exit 1
 fi
 
 $jtPath config $jail --set setNetAccess "true" >/dev/null || exit 1
 
-if ! $jtPath start $jail sh -c 'wget --spider -q https://linux.org' 2>/dev/null; then
+if ! $jtPath start $jail sh -c 'printf "GET / HTTP/1.1\r\nHost: kernel.org\r\n\r\n" | timeout 2 ssl_client kernel.org | grep -q ".*"' 2>/dev/null; then
 	echo "Could not connect to the remote site linux.org"
 	exit 1
 fi
