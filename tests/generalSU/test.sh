d191d66ad099297cbf48ba7014dec77dee01f115
Nicholas Niro <blowfist@xroutine.net>
2022-03-06 18:18:52 -0500

    tests/generalSU: rewrote the test to the new methodology.

--- tests/generalSU/test.sh
+++ tests/generalSU/test.sh
@@ -12,35 +12,44 @@ bb=$testPath/../bin/busybox
 
 . $testPath/../../utils/utils.sh
 
-$jtPath new $jail 2>/dev/null || exit 1
+$jtPath new $jail 2>/dev/null 2>/dev/null || exit 1
 
 uid=$(id -u)
 
 jUid=$(lift $jtPath start $jail id -u 2>/dev/null)
 
-echo "jail UID must be the user's UID -- user id : $uid ---- jail user id : $jUid"
-[ "$uid" != "$jUid" ] && exit 1
+if [ "$uid" != "$jUid" ]; then
+	echo "jail UID must be the user's UID -- user id : '$uid' ---- jail user id : '$jUid'"
+	exit 1
+fi
 
-echo "jail UID must not be the root UID"
-[ "$jUid" = "0" ] && exit 1
+if [ "$jUid" = "0" ]; then
+	echo "jail UID must not be the root UID"
+	exit 1
+fi
 
 # check the realRootInJail config which is supposed to provide in jail root.
 # Of course, for an unprivileged instance we only expect the fake root.
 
-echo "Setting the configuration : realRootInJail"
+# Setting the configuration : realRootInJail
 
 $jtPath config $jail -s realRootInJail true 2>/dev/null
 
 jUid=$(lift $jtPath start $jail id -u 2>/dev/null)
 
-echo "jail UID must be the root UID"
-[ "$jUid" = "0" ] || exit 1
+if [ ! "$jUid" = "0" ]; then
+	echo "jail UID must be the root UID"
+	exit 1
+fi
 
-echo "Doing a test by making a directory, changing it's ownership to root and checking it"
+# Doing a test by making a directory, changing it's ownership to root and checking it
 $jtPath start $jail sh -c 'mkdir /home/testDir' 2>/dev/null
 lift $jtPath start $jail chown root /home/testDir 2>/dev/null || exit 1
-echo "user owning the directory : '$($bb stat -c %U $jail/root/home/testDir)' (expecting 'root')"
-$bb stat -c %U $jail/root/home/testDir | grep -q root || exit 1
+
+if ! $bb stat -c %U $jail/root/home/testDir | grep -q root; then
+	echo "user owning the directory : '$($bb stat -c %U $jail/root/home/testDir)' (expecting 'root')"
+	exit 1
+fi
 
 # test jt itself, embedded in busybox
 
