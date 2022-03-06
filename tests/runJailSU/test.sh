10e725da0662dc5c61e60bddd9c6567ea262a4a8
Nicholas Niro <blowfist@xroutine.net>
2022-03-06 11:48:39 -0500

    tests/runJailSU: removed unnecessary outputs.

--- tests/runJailSU/test.sh
+++ tests/runJailSU/test.sh
@@ -13,20 +13,29 @@ bb=$testPath/../bin/busybox
 $jtPath new $testPath/basic 2>&1 || exit 1
 cd $testPath/basic
 
-echo Starting a daemon
+#echo Starting a daemon
 lift $jtPath daemon $testPath/basic 2>/dev/null || exit 1
-echo "Started the jail, now we check if it's actually running"
+# echo "Started the jail, now we check if it's actually running"
 $bb timeout 5 sh -c 'while :; do if [ -e run/jail.pid ]; then break; fi ; done'
 
-echo "checking to see if the jail is running"
+# checking to see if the jail is running
 if [ ! -e run/jail.pid ]; then
 	echo "The daemonized jail is not running, run/jail.pid is missing"
 	exit 1
 fi
 
-echo "Attempting to re-enter the daemonized jail"
-echo exit | $jtPath shell 2>&1 || exit 1
-echo "Stopping the daemonized jail"
+if ! echo exit | $jtPath shell 2>&1; then
+	echo "Attempt to re-enter the daemonized jail failed"
+	exit 1
+fi
+
+# attempting to re-enter the daemonized jail as root
+if ! lift $jtPath shell 2>&1; then
+	echo "Unable to rejoin jail as root"
+	exit 1
+fi
+
+# Stopping the daemonized jail
 lift $jtPath stop $testPath/basic 2>&1 || exit 1
 sleep 1
 
