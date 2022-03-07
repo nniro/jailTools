87db2d822cea446bbf2ec7fc9b00ff6210c999f1
Nicholas Niro <blowfist@xroutine.net>
2022-03-07 17:25:20 -0500

    tests/firewallSu: blockAllTest added a rule deletion test.

--- tests/firewallSU/blockAllTest.sh
+++ tests/firewallSU/blockAllTest.sh
@@ -100,4 +100,42 @@ if ! lift $jtPath start $jail sh /home/singleTest.sh /home/firewallCmd /home/fir
 fi
 # blockAll duplication with the instruction file removed end
 
+# rule deletion test
+cat - > $jail/root/home/firewallCmd << EOF
+sh /home/firewallFront.sh \$fwInstrPath firewall blockAll fwTestIn fwTestIn
+EOF
+
+cat - > $jail/root/home/firewallExpectedInstr << EOF
+firewall /tmp/firewallInstructions.txt external blockAll fwTestIn fwTestIn;
+EOF
+
+cat - > $jail/root/home/firewallDeleteCmd << EOF
+sh /home/firewallFront.sh \$fwInstrPath firewall -d blockAll fwTestIn fwTestIn
+EOF
+
+cat - > $jail/root/home/firewallExpectedDeleteInstr << EOF
+EOF
+
+cat - > $jail/root/home/firewallExpectedIptablesRules << EOF
+-A INPUT -i fwTestIn -p tcp -m tcp --dport 1:65535 -m state ! --state RELATED,ESTABLISHED -j REJECT --reject-with icmp-port-unreachable
+-A INPUT -i fwTestIn -p udp -m udp --dport 1:65535 -m state ! --state RELATED,ESTABLISHED -j REJECT --reject-with icmp-port-unreachable
+-A OUTPUT -o fwTestIn -m state ! --state RELATED,ESTABLISHED -j REJECT --reject-with icmp-port-unreachable
+EOF
+
+cat - > $jail/root/home/runTest.sh << EOF
+#! /bin/sh
+
+sh /home/singleTest.sh /home/firewallCmd /home/firewallExpectedInstr /home/firewallExpectedIptablesRules
+
+# Now we delete the inserted rules
+
+sh /home/singleTest.sh -d /home/firewallDeleteCmd /home/firewallExpectedDeleteInstr /home/firewallExpectedIptablesRules
+EOF
+
+if ! lift $jtPath start $jail sh /home/runTest.sh 2>/dev/null; then
+	echo "blockAll deletion test failed"
+	exit 1
+fi
+# rule deletion test end
+
 exit 0
