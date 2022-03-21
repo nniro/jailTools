a4766a277423ccea60d04d02819fda2684da7c0b
Nicholas Niro <blowfist@xroutine.net>
2022-03-20 22:04:43 -0400

    tests/runJailSU: Made a cleanup.

--- tests/runJailSU/test.sh
+++ tests/runJailSU/test.sh
@@ -10,33 +10,25 @@ bb=$testPath/../bin/busybox
 
 . $testPath/../../utils/utils.sh
 
-$jtPath new $testPath/basic >/dev/null 2>/dev/null || exit 1
-cd $testPath/basic
+jail=$testPath/basic
 
-#echo Starting a daemon
-lift $jtPath daemon $testPath/basic 2>/dev/null || exit 1
-# echo "Started the jail, now we check if it's actually running"
-$bb timeout 5 sh -c 'while :; do if [ -e run/jail.pid ]; then break; fi ; done'
+$jtPath new $jail >/dev/null 2>/dev/null || exit 1
 
-# checking to see if the jail is running
-if [ ! -e run/jail.pid ]; then
-	echo "The daemonized jail is not running, run/jail.pid is missing"
-	exit 1
-fi
+lift $jtPath daemon $jail 2>/dev/null || exit 1
 
-if ! echo exit | $jtPath shell 2>/dev/null; then
+if ! echo exit | $jtPath shell $jail 2>/dev/null; then
 	echo "Attempt to re-enter the daemonized jail failed"
 	exit 1
 fi
 
 # attempting to re-enter the daemonized jail as root
-if ! lift $jtPath shell 2>/dev/null; then
-	echo "Unable to rejoin jail as root"
+if ! echo exit | lift $jtPath shell $jail 2>/dev/null; then
+	echo "Reentry into the jail should be possible as root but it is not."
 	exit 1
 fi
 
 # Stopping the daemonized jail
-lift $jtPath stop $testPath/basic 2>&1 || exit 1
+lift $jtPath stop $jail 2>&1 || exit 1
 sleep 1
 
 exit 0
