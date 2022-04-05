849a4b3b23606a3a1f95ef3407431291a5aea463
Nicholas Niro <blowfist@xroutine.net>
2022-04-05 09:57:38 -0400

    tests/processStatus: Rewrote part of the test, made some changes and fixed a major bug in it.
    This test would intermittently pass and fail. This was because of the
    check on the process pid having different size of pid which were not
    handled correctly by the regex expression.

--- tests/processStatus/test.sh
+++ tests/processStatus/test.sh
@@ -14,6 +14,7 @@ jtPath=$3
 jail=$testPath/processStatus
 
 $jtPath new $jail >/dev/null 2>/dev/null || exit 1
+$jtPath config $jail -s setNetAccess false >/dev/null
 
 cd $jail
 
@@ -22,19 +23,30 @@ $jtPath daemon >/dev/null 2>/dev/null || exit 1
 # we check the status command itself
 # which is used to figure if a jail is running or not.
 
-if [ -e $jail/run/ns.pid ] \
-	&& [ -e $jail/run/jail.pid ]\
-	&& $bb ps | grep -q "^$(cat $jail/run/ns.pid) "\
-	&& $bb ps | grep -q "^$(cat $jail/run/jail.pid) "\
-	; then
-	# at this point, we know that the jail is running or we need to make sure that we 100% know.
+if [ -e $jail/run/ns.pid ] && [ -e $jail/run/jail.pid ]; then
+	psOutput="$($bb ps)"
+	if	printf "%s" "$psOutput" | grep -q "^ *$(cat $jail/run/ns.pid) "\
+		&& printf "%s" "$psOutput" | grep -q "^ *$(cat $jail/run/jail.pid) "; then
+		# at this point, we know that the jail is running or we need to make sure that we 100% know.
 
-	if ! $jtPath status ; then
-		echo "We know the jail is running but it is not detected as so."
+		if ! $jtPath status ; then
+			echo "We know the jail is running but it is not detected as so."
+			exit 1
+		fi
+	else
+		echo "The jail has the necessary pid files but we could not detect any process running from them."
+		echo "We are looking for pids : $(cat $jail/run/ns.pid) and $(cat $jail/run/jail.pid)"
+		echo "here is the full ps output :"
+		printf "%s" "$psOutput\n"
+		echo "here is the filtered ps output :"
+		printf "%s" "$psOutput" | grep "^ *\($(cat $jail/run/ns.pid)\|$(cat $jail/run/jail.pid)\) "
+
+		$jtPath stop $jail 2>/dev/null || exit 1
 		exit 1
 	fi
 else
 	echo "We expect the jail to be running at this point but it is not"
+	$jtPath stop $jail 2>/dev/null || exit 1
 	exit 1
 fi
 
