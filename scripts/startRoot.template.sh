69d9ffed3397289560fe8cff7d37fde85aa2d1c0
Nicholas Niro <blowfist@xroutine.net>
2022-04-09 13:58:23 -0400

    startRoot: Removed stopOnError because that would not have the effect wanted.
    The problem with it is that it would trigger a stop of a jail in the
    situation where we attempt to start an already started jail. In that
    case, we expect the second start attempt to fail and leave the jail
    running. But due to stopOnError, it would actually stop the running
    jail. So baring a better way to stopOnError, it was removed in this
    commit.

--- scripts/startRoot.template.sh
+++ scripts/startRoot.template.sh
@@ -54,13 +54,6 @@ prepareCmd() {
 	echo $result
 }
 
-stopOnError() {
-	ownPath=$1
-
-	stopChroot $ownPath
-	exit 1
-}
-
 cmdParse() {
 	local args=$1
 	local ownPath=$2
@@ -72,7 +65,7 @@ cmdParse() {
 			echo "This command is not meant to be called directly, use the jailtools super script to start the daemon properly, otherwise it will just stay running with no interactivity possible." >&2
 			jArgs="-d"
 			[ "$realRootInJail" = "true" ] && jArgs="$jArgs -r"
-			prepareChroot $ownPath || stopOnError $ownPath
+			prepareChroot $ownPath || exit 1
 			sleep 1
 			runJail $jArgs $ownPath $(prepareCmd "$runEnvironment" "$daemonCommand" "$@")
 			err=$?
@@ -83,7 +76,7 @@ cmdParse() {
 		start)
 			jArgs=""
 			[ "$realRootInJail" = "true" ] && jArgs="$jArgs -r"
-			prepareChroot $ownPath || stopOnError $ownPath
+			prepareChroot $ownPath || exit 1
 			sleep 1
 			runJail $jArgs $ownPath $(prepareCmd "$runEnvironment" "$startCommand" "$@")
 			err=$?
